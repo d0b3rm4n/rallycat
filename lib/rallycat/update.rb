43993fbe7ba98887616f7096cb31a00235080490
Emilio Cavazos <ejcavazos@gmail.com>
2012-07-17 09:44:47 -0700

    refactored and added a comment for task owner logic

--- lib/rallycat/update.rb
+++ lib/rallycat/update.rb
@@ -15,9 +15,14 @@ module Rallycat
         attributes[:blocked] = false
       end
 
-      user_name = attributes[:owner]
-
-      if user_name
+      # The value in attributes[:owner] should be equal to the desired owner's 
+      # display name in Rally. We need to fetch the user in order to get their
+      # login_name. The login_name is needed in order to set the owner
+      # attribute of a task.
+      # 
+      # We decided it would be easier for the user to enter 'John Smith'
+      # instead of 'john.smith@foobar.com'.
+      if user_name = attributes[:owner]
         user_results = @api.find(:user) do
           equal :display_name, user_name
         end
@@ -28,9 +33,17 @@ module Rallycat
 
       messages = []
 
-      messages << %{Task (#{task_number}) was set to "#{attributes[:state]}".} if attributes[:state]
-      messages << "Task (#{task_number}) was blocked." if attributes[:blocked]
-      messages << %{Task (#{task_number}) was assigned to "#{user_name}".} if attributes[:owner]
+      if attributes[:state]
+        messages << %{Task (#{task_number}) was set to "#{attributes[:state]}".}
+      end
+
+      if attributes[:blocked]
+        messages << "Task (#{task_number}) was blocked."
+      end
+
+      if attributes[:owner]
+        messages << %{Task (#{task_number}) was assigned to "#{user_name}".}
+      end
 
       messages.join("\n")
     end
