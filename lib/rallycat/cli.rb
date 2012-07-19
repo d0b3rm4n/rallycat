3b2412156bbe270d165676d0fbeefdae11ec5e58
Emilio Cavazos <ejcavazos@gmail.com>
2012-07-19 12:45:54 -0700

    Make parse methods private.

--- lib/rallycat/cli.rb
+++ lib/rallycat/cli.rb
@@ -12,6 +12,49 @@ module Rallycat
       parse_command_options!
     end
 
+    def run
+      case @command
+      when 'cat'
+        api = Rallycat::Connection.new(options[:username], options[:password]).api
+
+        story_number = @argv.shift
+
+        abort 'The "cat" command requires a story or defect number.' unless story_number
+
+        begin
+          @stdout.puts Rallycat::Cat.new(api).story(story_number)
+        rescue Rallycat::Cat::StoryNotFound => e
+          abort e.message
+        end
+      when 'update'
+        api = Rallycat::Connection.new(options[:username], options[:password]).api
+
+        task_number = @argv.shift
+
+        abort 'The "update" command requires a task number.' unless task_number
+
+        opts = {}
+        opts[:blocked] = true            if options[:blocked]
+        opts[:state]   = "In-Progress"   if options[:in_progress]
+        opts[:state]   = "Completed"     if options[:completed]
+        opts[:state]   = "Defined"       if options[:defined]
+        opts[:owner]   = options[:owner] if options[:owner]
+
+        begin
+          @stdout.puts Rallycat::Update.new(api).task(task_number, opts)
+        rescue Rallycat::Update::UserNotFound, Rallycat::Update::TaskNotFound => e
+          abort e.message
+        end
+      when 'help'
+        # `puts` calls `to_s`
+        @stdout.puts Rallycat::Help.new
+      else
+        @stdout.puts "'#{@command}' is not a supported command. See 'rallycat help'."
+      end
+    end
+
+    private
+
     def parse_global_options!
       @options ||= {}
 
@@ -72,47 +115,6 @@ module Rallycat
 
       @command = @argv.shift
       commands[@command].parse! @argv if commands.has_key? @command
-    end
-
-    def run
-      case @command
-      when 'cat'
-        api = Rallycat::Connection.new(options[:username], options[:password]).api
-
-        story_number = @argv.shift
-
-        abort 'The "cat" command requires a story or defect number.' unless story_number
-
-        begin
-          @stdout.puts Rallycat::Cat.new(api).story(story_number)
-        rescue Rallycat::Cat::StoryNotFound => e
-          abort e.message
-        end
-      when 'update'
-        api = Rallycat::Connection.new(options[:username], options[:password]).api
-
-        task_number = @argv.shift
-
-        abort 'The "update" command requires a task number.' unless task_number
-
-        opts = {}
-        opts[:blocked] = true            if options[:blocked]
-        opts[:state]   = "In-Progress"   if options[:in_progress]
-        opts[:state]   = "Completed"     if options[:completed]
-        opts[:state]   = "Defined"       if options[:defined]
-        opts[:owner]   = options[:owner] if options[:owner]
-
-        begin
-          @stdout.puts Rallycat::Update.new(api).task(task_number, opts)
-        rescue Rallycat::Update::UserNotFound, Rallycat::Update::TaskNotFound => e
-          abort e.message
-        end
-      when 'help'
-        # `puts` calls `to_s`
-        @stdout.puts Rallycat::Help.new
-      else
-        @stdout.puts "'#{@command}' is not a supported command. See 'rallycat help'."
-      end
     end
   end
 end
